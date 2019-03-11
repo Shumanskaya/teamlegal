87c7d6482cb52b7b98c668fb4a02e1f8d857b0e3
Evgenia <myemail@mail.ru>
2019-03-11 23:57:41 +0300

    внесены все тексты + правки по мобильному + правки по правкам + добаление нескольких цветов

--- my.js
+++ my.js
@@ -1,1 +1,14 @@
- 
+document.addEventListener("DOMContentLoaded", function (event) {
+    var button = document.getElementById('buttonMenu');
+
+    button.onclick = function () {
+        var menu = document.getElementById('menuMobile');
+        if (menu.style.display === 'none') {
+            menu.style.display = 'flex'
+        }
+        else {
+            menu.style.display = 'none' 
+        }
+    }
+
+});
