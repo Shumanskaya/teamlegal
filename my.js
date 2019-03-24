79c2510606f72cc2dee19397e79e71d8665dd535
Evgenia <shumanskaya@bk.ru>
2019-03-24 23:42:32 +0300

    добавила анимацию и логотип

--- my.js
+++ my.js
@@ -4,11 +4,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     button.onclick = function () {
         var menu = document.getElementById('menuMobile');
         if (menu.style.display === 'none') {
-            menu.style.display = 'flex'
+            menu.style.display = 'flex';
         }
         else {
-            menu.style.display = 'none' 
+            menu.style.display = 'none';
         }
     }
 
+    $('.header-top-menu__item_link').click(function (e) {
+        var href = $(this).attr('href');
+        var posHref = $(href).position();
+        e.preventDefault()
+        console.log(href)
+        console.log(posHref.top)
+        $('body').animate({
+            scrollTop: posHref.top
+        }, 1000)
+    })
+
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 500) {
+            $('.legal-scroll').fadeIn()
+        }
+        else {
+            $('.legal-scroll').fadeOut()
+        }
+    })
+
+    $('.legal-scroll').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000)
+    })
 });
